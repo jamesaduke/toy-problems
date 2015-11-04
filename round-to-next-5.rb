6274439962fcda3960f517803745d8affe4b9418
James Aduke <jamesodwuor4@gmail.com>
2015-11-04 23:37:06 +0300

    Partly solved

---
+++ round-to-next-5.rb
@@ -0,0 +1,16 @@
+# Given an integer as input, can you round it to the next 5?
+
+# Examples:
+
+# input:    output:
+# 0    ->   0
+# 2    ->   5
+# 3    ->   5
+# 12   ->   15
+# 21   ->   25
+# 30   ->   30
+# etc.
+
+def round_to_next_5(n)
+	 n.round(0.5)
+end
