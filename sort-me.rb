fd6194c5c3c5a79ef466542bea12a9b4758c1bc3
James Aduke <jamesodwuor4@gmail.com>
2015-12-10 10:35:44 +0300

    Add sort solution

---
+++ sort-me.rb
@@ -0,0 +1,13 @@
+# sort array by last character
+
+# write a function sortMe (sort_me in Python) to sort a given array by last character of the element. Element can be an integer or a string.
+
+# Example:
+
+# sortMe(['acvd','bcc']) => ['bcc','acvd'] As the last characters of strings are d and c. As, c comes before d, sorting by last character will give ['bcc', 'acvd']
+
+# if two elements don't differ in the last character, that then they should be sorted by the order they come in the array.
+def sortMe(arr)
+	arr.reverse!
+end
+p sortMe(["13", 14, "bsde", "asdf"])
