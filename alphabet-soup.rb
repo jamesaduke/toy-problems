8c57d0a2c1a9d8b096f3789f073484537278f563
James Aduke <jamesodwuor4@gmail.com>
2015-12-12 22:54:14 +0300

    Added alphabet soup solution

---
+++ alphabet-soup.rb
@@ -0,0 +1,15 @@
+# Using the Ruby language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.
+def AlphabetSoup(str)
+	array = str.split("")
+  	array.sort.to_s
+  # code goes here
+   
+         
+end
+
+#These are tests to check if the code is working
+p AlphabetSoup("Your argument goes here ")
+p AlphabetSoup("period")
+p AlphabetSoup("periods points")
